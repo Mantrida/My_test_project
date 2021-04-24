d9849caa7fe31caedb25ae21a1ff067a63abc193
Mantrida <Lexx47@mail.ru>
2021-04-24 17:28:03 +0300

    refactoring conftest.py

--- conftest.py
+++ conftest.py
@@ -1,23 +1,28 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+    """Опции командной строки.
+    В командную строку передается параметр вида '--language="es"'
+    По умолчанию передается параметр, включающий английский интерфейс в браузере
+    """
+    parser.addoption('--language', action='store', default='en', help='Choose language')
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    # В переменную user_language передается параметр из командной строки
+    user_language = request.config.getoption('language')
+
+    # Инициализируются опции браузера
+    options = Options()
+
+    # В опции вебдрайвера передаем параметр из командной строки
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+
+    browser.implicitly_wait(5)
     yield browser
-    print("\nquit browser..")
     browser.quit()
