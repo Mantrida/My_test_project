229be78bc8ec4faadb9ed8003e669e39c0acfc98
Mantrida <Lexx47@mail.ru>
2021-04-29 22:45:57 +0300

    Refactoring BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from .locators import BasePageLocators
+
 import math
 
 class BasePage:
@@ -8,7 +10,7 @@ class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -50,3 +52,9 @@ class BasePage:
         except NoAlertPresentException:
             print("No second alert presented")
 
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- pages/locators.py
+++ pages/locators.py
@@ -14,3 +14,7 @@ class ProductPageLocators:
     COST_ITEM = (By.CSS_SELECTOR, "div.product_main p.price_color")
     ALERT_INNER_ITEM = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong")
     ALERT_INNER_COST = (By.CSS_SELECTOR, "#messages div:nth-child(3) strong")
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
