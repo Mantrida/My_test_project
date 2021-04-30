fb13523f17db42ffd3ce5f8d90f74fcff47c1c11
Mantrida <Lexx47@mail.ru>
2021-04-30 14:18:47 +0300

    Add test test_guest_cant_see_product_in_basket_opened_from_main_page

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,20 +1,13 @@
 from .base_page import BasePage
-from .locators import LoginPageLocators
+from .locators import BasketPageLocators
 
-class LoginPage(BasePage):
-    def should_be_login_page(self):
-        self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
+class BasketPage(BasePage):
+    def basket_should_be_empty(self):
+        self.basket_not_empty()
+        self.basket_empty()
 
-    def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert "login" in self.browser.current_url
+    def basket_not_empty(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_CONTENT), "Content is presented, but should not be"
 
-    def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
-
-    def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+    def basket_empty(self):
+        assert self.is_element_present(*BasketPageLocators.CONTENT_INNER), "Massage is not presented"
--- pages/locators.py
+++ pages/locators.py
@@ -12,9 +12,14 @@ class ProductPageLocators:
     BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
     NAME_ITEM = (By.CSS_SELECTOR, "div.product_main h1")
     COST_ITEM = (By.CSS_SELECTOR, "div.product_main p.price_color")
-    ALERT_INNER_ITEM = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong")
-    ALERT_INNER_COST = (By.CSS_SELECTOR, "#messages div:nth-child(3) strong")
+    ALERT_INNER_ITEM = (By.CSS_SELECTOR, "#messages > .alert-success > .alertinner > strong")
+    ALERT_INNER_COST = (By.CSS_SELECTOR, "#messages > .alert-info > .alertinner > p > strong")
 
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn-default")
+
+class BasketPageLocators:
+    CONTENT_INNER = (By.CSS_SELECTOR, "#content_inner p")
+    BASKET_CONTENT = (By.CSS_SELECTOR, "#basket_formset")
